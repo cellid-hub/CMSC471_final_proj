4eef2ef94121771c8c4561562781ec03bf4b7a28
Dylan-Celli <cellidylan@gmail.com>
2023-05-06 21:40:59 -0400

    Adding para coords progress

--- main.js
+++ main.js
@@ -237,3 +237,81 @@ function update_Yaxis() {
         updateChart()
     }
 }
+
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 20, left: 20},
+  width = 500 - margin.left - margin.right,
+  height = 600 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg2 = d3.select("#my_dataviz")
+.append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+.append("g")
+  .attr("transform",
+        "translate(" + margin.left+ "," + margin.right + ")");
+
+// Parse the Data
+d3.csv("country_means.csv").then(function(data) {
+
+  // Extract the list of dimensions we want to keep in the plot. Here I keep all except the column called Species
+  dimensions = data.columns;  
+  dimensions = dimensions.filter(function(d) { return d != "Country";})  ;
+  // For each dimension, I build a linear scale. I store all in a y object
+  svg2.selectAll("select.axis_selector").selectAll("option").data(dimensions).enter().append("option")
+	.each(function(d){                 
+                d3.select(this).text(d).attr('value',d);
+        });       
+  var y = {}
+  for (i in dimensions) {
+    name = dimensions[i]
+    y[name] = d3.scaleLinear()
+      .domain( d3.extent(data, function(d) { return +d[name]; }) )
+      .range([height, 0])
+  }
+
+  // Build the X scale -> it find the best position for each Y axis
+  x = d3.scalePoint()
+    .range([0, width])
+    .padding(1)
+    .domain(dimensions);
+
+  // The path function take a row of the csv as input, and return x and y coordinates of the line to draw for this raw.
+  function path(d) {
+      return d3.line()(dimensions.map(function(p) { return [x(p), y[p](d[p])]; }));
+  }
+
+  // Draw the lines
+  svg2
+    .selectAll("myPath")
+    .data(data)
+    .enter().append("path")
+    .attr("d",  path)
+    .style("fill", "none")
+    .style("stroke", "#69b3a2")
+    .style("opacity", 0.5)
+  var i = 0;
+  // Draw the axis:
+  svg2.selectAll("myAxis")
+    // For each dimension of the dataset I add a 'g' element:
+    .data(dimensions).enter()
+    .append("g")
+    // I translate this element to its right position on the x axis
+    .attr("transform", function(d) { return "translate(" + x(d) + ")"; })
+    // And I build the axis with the call function
+    .each(function(d) { d3.select(this).call(d3.axisLeft().scale(y[d])); })
+    // Add axis title
+    .append("text")
+	  .style("text-anchor", "middle")
+	  .attr("y", function() 
+			{ if( i % 2 == 0){
+				i += 1;
+				return -9;
+			}
+			i += 1;
+			return 575;})
+	  .text(function(d) { return d; })
+	  .style("fill", "blue");
+
+})
